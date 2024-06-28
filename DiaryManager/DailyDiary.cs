867229cd23f1f8f543b6e2550a29920ffadfd640
Ja'far Ramadan <23040271@student.ltuc.com>
2024-06-28 22:34:02 +0300

    Add project files.

---
+++ DiaryManager/DailyDiary.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaryManager
+{
+    public class DailyDiary
+    {
+        //read
+        public static string ReadAllTextMethod(string filepathParam)
+        {
+            string txtContent = File.ReadAllText(filepathParam);
+            return txtContent;
+
+        }
+        //add
+        public static string[] AddMethod(string filepathParam, Entry date, Entry content)
+        {
+            if (DateTime.TryParseExact(date.Date, "yyyy-MM-dd", null, DateTimeStyles.None, out DateTime parsedDate))
+            {
+                string formattedDate = parsedDate.ToString("yyyy-MM-dd");
+                string addedContent = $"\n {formattedDate} \n {content.Content}";
+                File.AppendAllText(filepathParam, addedContent);
+                string[] strings = File.ReadAllLines(filepathParam);
+                return strings;
+            }else
+            {
+                return new string[0];   
+            }
+
+        }
+
+        //delete
+        public static void DeleteMethod(string filepathParam, Entry content)
+        {
+            string txtContent = File.ReadAllText(filepathParam);
+            txtContent= txtContent.Replace(content.Content, "");
+            File.WriteAllText(filepathParam, txtContent);
+        }
+
+        //count
+        public static int CountMethod(string filepathParam)
+        {
+            string [] number=File.ReadAllLines(filepathParam);
+            return number.Length;
+
+        }
+
+        //readByDate
+        public static void readByDateMethod(string filepathParam, Entry date)
+        {
+            if (DateTime.TryParseExact(date.Date, "yyyy-MM-dd", null, DateTimeStyles.None, out DateTime parsedDate))
+            {
+                string formattedDate = parsedDate.ToString("yyyy-MM-dd");
+              string [] lines=  File.ReadAllLines(filepathParam);
+                for (int i = 0; i < lines.Length; i++) 
+                { 
+                    if (lines[i]== formattedDate)
+                    {
+                        Console.WriteLine(lines[i+1]);
+                    }
+                }
+
+
+            }
+        }
+    }
+}
---
+++ DiaryManager/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace DiaryManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string filepath = Path.Combine(Environment.CurrentDirectory, "mydiary.txt");
+
+
+
+                int option = 0;
+
+                while (option != 6)
+                {
+                    Console.WriteLine("\n1.Reading The Entiar Diary.\n2.Add Enties to Your Diary" +
+                    "\n3.Delete An Entries.\n4.Counting Numbers Of The Entries.\n5.Read Data For Specific date." +
+                   "\n6.Exit");
+                    option = int.Parse(Console.ReadLine());
+                    
+                    if (option == 1)
+                    {
+                        Console.WriteLine("File content:\n");
+                        Console.WriteLine(DailyDiary.ReadAllTextMethod(filepath));
+                    }
+                    else
+                if (option == 2)
+                    {
+                        Console.WriteLine("Write The date of your content.");
+                        string date = Console.ReadLine();
+                        Console.WriteLine("Write The your content.");
+                        string content = Console.ReadLine();
+                        DailyDiary.AddMethod(filepath, new Entry { Date = date }, new Entry { Content = content });
+                        Console.WriteLine(DailyDiary.ReadAllTextMethod(filepath));
+                    }
+                    else
+                if (option == 3)
+                    {
+                        Console.WriteLine("Write the content you want to Delete");
+                        string content = Console.ReadLine();
+                        DailyDiary.DeleteMethod(filepath, new Entry { Content = content });
+                        Console.WriteLine(DailyDiary.ReadAllTextMethod(filepath));
+                    }
+                    else
+                if (option == 4)
+                    {
+                        Console.WriteLine("your Entries Are Numbers Are:");
+                        
+                        Console.WriteLine(DailyDiary.CountMethod(filepath));
+                    }
+                    else if (option == 5)
+                    {
+                        Console.WriteLine("Enter Your Date to get its Entry");
+                        string date = Console.ReadLine();
+                        DailyDiary.readByDateMethod(filepath, new Entry { Date = date });
+                    }
+                    else
+                    if (option > 6)
+                    {
+                        {
+                            Console.WriteLine("Choose only from 1-6");
+                        }
+                    }
+                }
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("done!");
+            }
+        }
+    }
+}
